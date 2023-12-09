67b8924aa61e89fbcbe09c16d576d923c819fb41
Rob <robcodehub@gmail.com>
2023-12-09 15:50:37 +1100

    updates to banner and nav bar

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,17 +11,14 @@ export default function HeroSection() {
            style={{
              backgroundImage: `url('/img/herobanner.jpg')`, // And here
            }}>
-        <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
+        <span id="blackOverlay" className="w-full h-full absolute opacity-20 bg-white"></span>
       </div>
 
       <div className="container relative mx-auto" style={{ zIndex: 2 }}>
         <div className="items-center flex flex-wrap">
           <div className="w-full lg:w-8/12 px-4 ml-auto mr-auto text-center">
             <div className="pr-12">
-              {/* <img src="/img/logo.svg" alt="logo" className="mx-auto mb-6 w-80" />  */}
-              <h1 className="text-white font-semibold text-5xl">
-                Little Lemon
-              </h1>
+              <img src="/img/logo.svg" alt="logo" className="mx-auto mb-6 w-[30rem]" /> 
               {/* Star ratings and other hero content here */}
             </div>
           </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,18 @@ import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <nav className="absolute top-0 left-0 w-full z-10 bg-black bg-opacity-50">
-      <div className="container mx-auto flex items-center justify-between p-2 md:p-4">
-        <Link href="/" className="font-bold text-xl text-white">
-        <img src="/img/logo.svg" alt="logo" className="mx-auto my-3" /> 
+    <nav className="absolute top-0 left-0 w-full z-10 opacity-60 bg-black">
+      <div className="container mx-auto flex items-center justify-between p-2 md:p-4 z-20 ">
+        <Link href="/" className="font-bold text-xl text-black">
+        <img src="/img/logo.svg" alt="logo" className="mx-auto w-44 my-1" /> 
         </Link>
-        <div className="hidden md:flex items-center space-x-10 text-xl text-white font-semibold hover:text-yellow-300">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/menu">Menu</Link>
-          <Link href="/reservations">Reservations</Link>
-          <Link href="/order-online">Order Online</Link>
-          <Link href="/login">Login</Link>
+        <div className="hidden md:flex items-center space-x-10 text-xl text-yellow-500 font-semibold z-20 tracking-wider">
+          <Link href="/" className=" hover:text-white">Home</Link>
+          <Link href="/about" className=" hover:text-white">About</Link>
+          <Link href="/menu" className=" hover:text-yellow-500">Menu</Link>
+          <Link href="/reservations" className=" hover:text-white">Reservations</Link>
+          <Link href="/order-online" className=" hover:text-white">Order Online</Link>
+          <Link href="/login" className=" hover:text-white">Login</Link>
         </div>
       </div>
     </nav>
