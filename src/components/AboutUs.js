1c1186fb1462d820d1ca22911902d8492c44f9c7
Rob <robcodehub@gmail.com>
2023-12-16 09:47:01 +1100

    fixed about section for mobile

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,17 +6,18 @@ export default function AboutUs() {
         <div className="w-full md:w-1/2 h-full">
           <h2 className="text-3xl font-bold mb-4 text-primary-light">Little Lemon Chicago</h2>
           <p className="text-secondary-blush text-lg">
-          Little Lemon is a delightful neighborhood bistro nestled in the heart of the city, offering a sanctuary for those seeking the comfort of home-cooked meals and the spirit of a vibrant community. <br />  <br />
-          Our menu showcases a symphony of simple, yet exquisitely prepared dishes that honor the bounty of the region, accompanied by a curated selection of classic cocktails to elevate your dining experience. <br />  <br />
-          With an ambiance that buzzes with energy and warmth, Little Lemon is the perfect retreat for casual diners and food enthusiasts alike, eager to indulge in the pleasures of a well-laid table and the timeless ritual of shared meals.
+            Little Lemon is a delightful neighborhood bistro nestled in the heart of the city, offering a sanctuary for those seeking the comfort of home-cooked meals and the spirit of a vibrant community. <br />  <br />
+            Our menu showcases a symphony of simple, yet exquisitely prepared dishes that honor the bounty of the region, accompanied by a curated selection of classic cocktails to elevate your dining experience. <br />  <br />
+            With an ambiance that buzzes with energy and warmth, Little Lemon is the perfect retreat for casual diners and food enthusiasts alike, eager to indulge in the pleasures of a well-laid table and the timeless ritual of shared meals.
           </p>
         </div>
 
-        <div className="w-full md:w-1/2 flex justify-end content-end">
+        <div className="hidden md:flex md:w-1/2 justify-end content-end">
           <div className="w-3/4">
-            <img src="/img/atmosphere.jpg" alt="Top Right Image" className="rounded-full"/>
+            <img src="/img/atmosphere.jpg" alt="Top Right Image" className="rounded-full" />
           </div>
         </div>
+
       </div>
     </div>
   );
